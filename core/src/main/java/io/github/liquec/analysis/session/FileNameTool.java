0ec319b45ffb6f3af40dd9665341877e5dffd9be
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-06-24 11:33:45 +0200

    changed icon app and file extension

--- core/src/main/java/io/github/liquec/analysis/session/FileNameTool.java
+++ core/src/main/java/io/github/liquec/analysis/session/FileNameTool.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class FileNameTool {
-    public static final String SESSION_SUFFIX = ".liquec";
+    public static final String SESSION_SUFFIX = ".liq";
 
     private FileNameTool() {
         // Prevent instantiation - all methods are static
